6364458a876fe52b3ccfb165b95547d43f434936
jay ess <mentoce@gmail.com>
2023-06-02 17:06:19 -0700

    Extensive changes and it's finally working

--- pyautoserve.py
+++ pyautoserve.py
@@ -1,10 +1,10 @@
+import multiprocessing as mp
 import os
 import time
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
-from socketserver import ThreadingMixIn
-from threading import Event, Thread
-from typing import Callable, Generator, Hashable
+from socketserver import TCPServer
+from typing import Any, Callable, Generator, Hashable
 
 
 class HashRegistry:
@@ -65,6 +65,12 @@ class HashRegistry:
 
         return self._hash(value) == self._registry[key]
 
+    def __contains__(self, key: Hashable) -> bool:
+        return key in self._registry
+
+    def __getitem__(self, key: Hashable) -> Any:
+        return self._registry[key]
+
 
 class FileObserver:
 
@@ -83,42 +89,31 @@ class FileObserver:
             for file in files:
                 yield Path(os.path.join(root, file))
 
-    def observe(self, pattern: str = "*") -> Path:
+    def observe(self) -> list[Path]:
         """
         Observe the directory space for any file changes.
 
-        Args:
-            pattern (str, optional): The file pattern to observe. Defaults to
-            "*" which is all files.
-
         Returns:
-            Path: If the file has changed, then (the Path to the file will be
-            returned, otherwise None will be returned.
+            list[Path]: List of Path to changed files.
         """
-        for file in self.scan(pattern):
-            with open(file, "r") as f:
-                data = f.read()
+        changed_files = []
+        for file in self.scan():
+            last_changed = os.stat(file).st_mtime
+            if file not in self._registry:
+                self._registry.register(file, last_changed)
+                changed_files.append(file)
+            elif not self._registry.compare(file, last_changed):
+                self._registry.update(file, last_changed)
+                changed_files.append(file)
 
-            if not file.name in self._registry:
-                    self._registry.register(file, data)
-            else:
-                if not self._registry.compare(file, data):
-                    self._registry.update(file, data)
-                    return file
+        return changed_files
 
-        return None
 
-    def __contains__(self, key: Hashable) -> bool:
-        return key in self._registry
+class HummingbirdServer(HTTPServer):
+    pass
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-
-class Server(ThreadingMixIn, HTTPServer):
     pass
 
 
@@ -130,50 +125,71 @@ class AutoreloadHTTPServer:
         port: int = 8000,
         root: str = ".",
         delay: float = 0.001,
-        *args,
-        **kwargs
+        server_class: TCPServer = HummingbirdServer,
+        request_class: BaseHTTPRequestHandler = RequestHandler
     ) -> None:
         self._delay = delay
-        self._event = Event()
-        self._observer = FileObserver(root)
-        self._observer_thread = Thread(
-            name="observer", target=None, daemon=True
-        )
-        self._server = Server((host, port), RequestHandler, *args, **kwargs)
-        self._server_thread = Thread(
-            name="server", target=self._start, daemon=True
-        )
+        self._observer = FileObserver(Path(root))
+        self._httpd = server_class((host, port), request_class)
+        self._httpd_process = None
 
-    def start(self) -> None:
+    def serve(self) -> None:
+        """
+        Starts the server
+        """
+        self._start_server_process()
+        num_files = len(list(self._observer.scan()))
+        print(f"Watching {num_files} files.")
+        try:
+            just_loaded = True
+            while True:
+                changed_files = self._observer.observe()
+                if changed_files and not just_loaded:
+                    num_files = len(changed_files)
+                    verb = "has" if num_files == 1 else "have"
+                    noun = "file" if num_files == 1 else "files"
+                    print(f"{num_files} {noun} {verb} changed. Restarting server")
+                    self._restart_server_process()
+                if just_loaded:
+                    just_loaded = False
+                time.sleep(self._delay)
+        except KeyboardInterrupt as e:
+            self._stop_server_process()
+            print(e)
+
+    def _start_server_process(self) -> None:
         """
         Starts the server.
         """
-        self._server_thread.start()
+        port = self._httpd.server_port
+        host = self._httpd.server_name
+        print(f"Starting server using host {host} on port {port}")
+        self._httpd_process = self._new_server_process()
+        self._httpd_process.start()
 
-    def stop(self) -> None:
+    def _stop_server_process(self) -> None:
         """
         Stops the server
         """
         self._server.shutdown()
 
-    def restart(self) -> None:
+    def _restart_server_process(self) -> None:
         """
         Restarts the server
         """
-        self.stop()
-        self.start()
+        self._httpd_process.terminate()
+        self._start_server_process()
 
-    def run(self, pattern: str = "*") -> None:
-        self.start()
-        while True:
-            changed_file = self._observer.observe(pattern)
-            if changed_file:
-                print(f"{changed_file} has changed. Restarting server")
-                self.restart()
+    def _serve(self) -> None:
+        """
+        Starts the server.
+        """
+        self._httpd.serve_forever()
 
-            time.sleep(self._delay)
+    def _new_server_process(self) -> mp.Process:
+        return mp.Process(target=self._serve, name="hummingbird_server")
 
 
 if __name__ == "__main__":
     server = AutoreloadHTTPServer(port=5555)
-    server.run()
+    server.serve()
