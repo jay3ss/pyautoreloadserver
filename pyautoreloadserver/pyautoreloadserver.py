dff5f5fb3bda3af64ab8086776923938a16ab410
jay ess <mentoce@gmail.com>
2023-06-05 23:07:36 -0700

    Remove 'main'

--- pyautoreloadserver/pyautoreloadserver.py
+++ pyautoreloadserver/pyautoreloadserver.py
@@ -207,36 +207,3 @@ def create_request_handler_class(
             super().__init__(*args, directory=directory, **kwargs)
 
     return CustomRequestHandler
-
-
-if __name__ == "__main__": # pragma: no cover
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Run the server")
-    parser.add_argument(
-        "-r",
-        dest="root",
-        type=str,
-        help="The root directory to monitor",
-    )
-    parser.add_argument(
-        "-p",
-        action="store",
-        dest="port",
-        default=8000,
-        type=int,
-        help="The port to use",
-    )
-    parser.add_argument(
-        "-n",
-        action="store",
-        dest="name",
-        default="localhost",
-        type=str,
-        help="The host name to use",
-    )
-
-    args = parser.parse_args()
-
-    server = AutoreloadHTTPServer(port=args.port, root=args.root)
-    server.serve()
